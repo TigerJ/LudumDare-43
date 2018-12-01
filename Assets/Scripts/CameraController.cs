2ac08ec25db9ee9dd476871277c83b6505850707
TigerJ <jbriggs@leafsoftwaresolutions.com>
2018-11-30 23:50:38 -0500

    updated with camera smooothDamp

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-
+    GameObject target;
+    Vector3 velocity;
 	// Use this for initialization
 	void Start () {
-		
+        target = GameObject.Find("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector3 playerTarget = new Vector3(target.transform.position.x, target.transform.position.y, -10f);
+        transform.position = Vector3.SmoothDamp(transform.position, playerTarget, ref velocity, .5f);
 	}
 }
