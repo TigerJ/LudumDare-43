8942d879caa2a54923ea0b7eceb3fbbd5d002abb
TigerJ <jbriggs@leafsoftwaresolutions.com>
2018-11-30 23:40:09 -0500

    adding points of interest, updated ui and added player targeting + movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    GameObject target;
+    bool startMove;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (startMove == true)
+        {
+            Debug.Log("start moving");
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, .05f);
+        }
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "poi")
+        {
+            startMove = false;
+        }
+    }
+    public void setTarget(GameObject gameObject)
+    {
+        target = gameObject;
+        startMove = true;
+    }
+}
---
+++ Assets/Scripts/PointOfInterest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointOfInterest : MonoBehaviour {
+    Player player;
+    private void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
+    private void OnMouseDown()
+    {
+        player.setTarget(gameObject);
+    }
+}
